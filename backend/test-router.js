f622ff02771192e1f555503934dc4332fc75d67e
Berna ÇAKIR <101141003+berinackr@users.noreply.github.com>
2025-12-31 11:51:08 +0300

    Delete backend directory

--- backend/test-router.js
+++
@@ -1,6 +0,0 @@
-// Simple script to test if router exports correctly
-const customerRoutes = require('./routes/customers');
-
-console.log('customerRoutes type:', typeof customerRoutes);
-console.log('customerRoutes is router?', customerRoutes && customerRoutes.stack ? true : false);
-console.log('customerRoutes details:', customerRoutes);
